6812e766081d3b9d88ebf5dceeab31a26d77b400
paolofullone <paolo.fullone@gmail.com>
2022-03-20 19:15:52 -0300

    timer xp mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './logo-xpi.svg';
+import Header from '../src/Components/header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const ONE_SECOND = 1000;
+const TIME_LIMIT = 0;
+const TIME_START = 5 * 60;
+
+export default class Timer extends Component {
+  state = {
+    seconds: TIME_START,
+    isDecreasing: false,
+    stopped: false,
+  };
+
+  // if reaches 0, stop timer.
+  componentDidUpdate() {
+    const { seconds } = this.state;
+    if (seconds === TIME_LIMIT) {
+      this.stop();
+    }
+  }
+
+  // function to start countdown timer
+  countdown = () => {
+    this.timerId = setInterval(() => {
+      this.setState((prevState) => ({
+        seconds: prevState.seconds - 1,
+      }));
+    }, ONE_SECOND);
+    this.setState({
+      isDecreasing: true,
+      stopped: false,
+    })
+  }
+
+  // function to stop countdown
+  stop = () => {
+    clearInterval(this.timerId);
+    const { isDecreasing, stopped } = this.state;
+    this.setState({
+      isDecreasing: !isDecreasing,
+      stopped: !stopped,
+    })
+  }
+
+  reset = () => {
+    this.setState({
+      seconds: TIME_START,
+      stopped: false,
+    })
+  }
+
+  secs = () => {
+    const { seconds } = this.state;
+    return seconds % 60
+  }
+  mins = () => {
+    const { seconds } = this.state;
+    return Math.floor(seconds/60)
+    }
+
+  hours = () => {
+
+  }
+  
+  increase = () => {
+    this.setState((prevState) => ({
+      seconds: prevState.seconds + 60,
+    }))
+  }
+
+  decrease = () => {
+  this.setState((prevState) => ({
+    seconds: prevState.seconds - 60,
+  }))
 }
 
-export default App;
+  render() {
+    const { isDecreasing, stopped } = this.state;
+      return (
+        <div>
+          <Header />
+          <h2 className='container title'>Estamos só começando.</h2>
+          <section className="timer container">
+            <igm src={logo}/>
+            <span>{String(this.mins()).padStart(2, '0')}</span>
+            <span>:</span>
+            <span>{String(this.secs()).padStart(2, '0')}</span>
+          </section>
+          <div className="container">
+            <div>
+              <button className="btn" type="button" onClick={this.countdown} disabled={isDecreasing}>{!stopped ? 'Start' : 'Resume'}</button>
+              <button className="btn" type="button" onClick={ this.increase } disabled={isDecreasing}>+</button>
+              <button className="btn" type="button" onClick={this.decrease} disabled={isDecreasing}>-</button>
+            </div>
+          </div>
+          <div className='container'>
+            <div>
+              <button className="btn" type="button" onClick={this.stop} disabled={stopped}>Stop</button>
+              <button className="btn" type="button" onClick={ this.reset } disabled={isDecreasing}>Reset</button>
+          </div>
+          </div>
+
+        </div>
+      );
+    }
+}
---
+++ src/Components/header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+function index() {
+  return (
+    <div class="header">
+      <img src="https://web.xpi.com.br/wp-content/themes/xpi-acquisition-xp/assets/images/header/logo-xpi.svg" alt="XP Investimentos"/>
+    <div class="header-right">
+      <a class="active" href="https://www.xpi.com.br/" target="_blank" rel="noreferrer">XPI</a>
+      <a href="https://www.betrybe.com" target="_blank" rel="noreferrer">Trybe</a>
+      <a href="https://www.linkedin.com/in/paolofullone/" target="_blank" rel="noreferrer">Paolo</a>
+    </div>
+  </div>
+  )
+}
+
+export default index
