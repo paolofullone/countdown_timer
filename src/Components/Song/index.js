5d7786bf87340a1d092612dcf70aa81cb0e284bd
paolofullone <paolo.fullone@gmail.com>
2022-03-21 16:03:27 -0300

    disable play btn (if started have to listen 🤣)

--- src/Components/Song/index.js
+++ src/Components/Song/index.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 import song from '../../assets/Oceans.mp3'
 
 export default function Song() {
+  const [playing, setPlaying] = useState(false)
   const audio = new Audio(song);
 
   const start = () => {
-    audio.play();
+      audio.play();
+      setPlaying(true);
   };
 
   return (
     <div className="container">
-      <button className="btn" onClick={start}>Play</button>
+      <button className="btn" onClick={start} disabled={playing}>Play</button>
     </div>
   );
 }
